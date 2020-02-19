e52944f9046324aa3a61f365f003c5dc504e6029
Long Le <ldleknox.edu>
2020-02-19 11:16:13 -0600

    prefab

--- CS292-Template/Assets/Scripts/New Folder/PlayButton.cs
+++ CS292-Template/Assets/Scripts/New Folder/PlayButton.cs
@@ -4,11 +4,39 @@ using UnityEngine;
 
 public class PlayButton : MonoBehaviour
 {
-    public GameObject GameScreen;
-    public GameObject TitleScreen;
+	public GameObject GameScreen;
+	public GameObject TitleScreen;
+
+	public void OpenPanel()
+	{
+		/*if(GameScreen != null)
+        {
+            bool isActive = GameScreen.activeSelf;
+            GameScreen.SetActive(!isActive);
+        }*/
+		GameScreen.SetActive(true);
+		TitleScreen.SetActive(false);
+	}
+
+}
+/*=======
+﻿using System.Collections;
+=======
+﻿/*using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour
+{
+    public GameObject Panel;
     public void OpenPanel()
     {
-        GameScreen.SetActive(true);
-        TitleScreen.SetActive(false);
+        if(Panel != null){
+            bool isActive = Panel.activeSelf;
+            Panel.setActive(!isActive);
+        }
     }
 }
+
+*/

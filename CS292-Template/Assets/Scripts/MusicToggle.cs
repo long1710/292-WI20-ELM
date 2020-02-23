2497fafea3a61d9b14a274b018c15e359a056c43
eschartz <ejschartz@knox.edu>
2020-02-23 10:58:54 -0600

    Script for music toggle
    Created script for the music toggle on the menu screen.

---
+++ CS292-Template/Assets/Scripts/MusicToggle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class MusicToggle : MonoBehaviour
+
+{
+    public void changeMusic()
+	{
+        if (AudioListener.volume == 0) //if sound is off
+		{
+			AudioListener.volume = 100; //turn sound on
+		}
+
+        else //if sound is on
+		{
+			AudioListener.volume = 0; //turn sound off
+		}
+	}
+}

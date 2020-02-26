dd190014ed485515c8d88f0dfc13bf194ea62337
eschartz <ejschartz@knox.edu>
2020-02-26 12:55:45 -0600

    Fixed quit game button functionality
    Quit game button on game over screen now works correctly.

--- CS292-Template/Assets/Scripts/New Folder/QuitGameButton.cs
+++ CS292-Template/Assets/Scripts/New Folder/QuitGameButton.cs
@@ -8,20 +8,9 @@ public class QuitGameButton : MonoBehaviour
 	public GameObject CurrentPanel;
 	public GameObject GameScreen;
 
-	/*public void OpenPanel()
+    public void QuitGame()
 	{
-		if (PanelToOpen != null)
-		{
-			PanelToOpen.SetActive(true);
-			CurrentPanel.SetActive(false);
-			GameScreen.SetActive(false);
-		}
-	}*/
-
-	public void RestartGame()
-	{
-		StartCoroutine(playSound()); //call helper
-
+		StartCoroutine(playSound());
 	}
 
 	IEnumerator playSound()
@@ -30,6 +19,8 @@ public class QuitGameButton : MonoBehaviour
 		source.Play();
 		yield return new WaitWhile(() => source.isPlaying); //wait until sound has played
 															//do something
-		SceneManager.LoadScene("Gameplay"); // loads current scene
+		//SceneManager.LoadScene("Gameplay"); // loads current scene
+        Application.Quit();
+		Debug.Log("Game is exiting");
 	}
 }

45e877e5cfd620e1ad5f67417bcb0b35d82d3b96
Mujtaba Hassan <smhassan@knox.edu>
2020-02-26 10:58:20 -0600

    Commit

---
+++ CS292-Template/Assets/Scripts/MusicToggle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicToggle : MonoBehaviour
+{
+
+    private bool muted;
+    // Start is called before the first frame update
+   public void SetAudio()
+    {
+        if (muted == false)
+        {
+            AudioListener.volume = 0;
+            muted = true;
+        }
+
+        else
+        {
+            AudioListener.volume = 100;
+            muted = false;
+        }
+    }
+}
